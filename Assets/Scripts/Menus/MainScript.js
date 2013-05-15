039eedecbac13a69ecabfeb613015626feb03cfe
Patrick Rasmussen <mcman407@gmail.com>
2013-05-15 08:14:06 -0400

    Update

--- Assets/Scripts/Menus/MainScript.js
+++ Assets/Scripts/Menus/MainScript.js
@@ -3,16 +3,23 @@
 var intro: Transform;
 var title: Transform;
 var titleSpeed: int = .2;
+
 function Start () {
 title.position.y = -.7;
 }
-
+var count: int;
 function Update () {
-if(title.position.y <= .848){
-title.transform.Translate(Vector3.up * titleSpeed * Time.deltaTime);
-}
-else{
-titleSpeed = 0; 
-title.position.y = .848;
-}
+	count = 0;
+	if(title.position.y <= .848){
+	title.transform.Translate(Vector3.up * titleSpeed * Time.deltaTime);
+	}
+	else{
+	titleSpeed = 0; 
+	title.position.y = .848;
+	}
+	
+	if(Input.GetButton("Continue")|| Input.GetAxis("Continue")){
+	count++;
+	Application.LoadLevel(2);
+	}
 }
