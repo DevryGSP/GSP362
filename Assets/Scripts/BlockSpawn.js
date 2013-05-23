236aa3f602b60a8a1ae88613eb39a1a7ee3b8895
Alex Larioza <alexlarioza@gmail.com>
2013-05-23 11:14:02 -0700

    Added new blocks and pieces

--- Assets/Scripts/BlockSpawn.js
+++ Assets/Scripts/BlockSpawn.js
@@ -1,6 +1,6 @@
 #pragma strict
 
-var piecePrefab : GameObject;
+var pieces : GameObject[];
 var delay : Number = 2.0;
 var speed : Number = 2.0;
 var velocity : Vector2;
@@ -27,18 +27,24 @@ function Update ()
         timer = delay;
         //speed += 0.5; 
      	
+     	// disable player control of previous piece
      	if (currentPiece)
      	{
      		currentPiece.isSelected = false;
      	}
-     	         
-    	var o : GameObject = Instantiate(piecePrefab, this.transform.position, this.transform.rotation);
-    	var piece : Piece = o.GetComponent("Piece") as Piece;
+     	
+     	// randomly choose a piece
+     	var prefab:GameObject = pieces[Random.Range(0, pieces.Length)];
+     	
+     	// spawn piece
+    	var o:GameObject = Instantiate(prefab, this.transform.position, this.transform.rotation);
+    	var piece:Piece = o.GetComponent("Piece") as Piece;
     	piece.isSelected = true;
     	piece.speed = speed;
     	piece.velocity.x = this.velocity.x;  
     	piece.velocity.y = this.velocity.y;
     	
+    	// hold reference to piece
     	currentPiece = piece;
     }
     
