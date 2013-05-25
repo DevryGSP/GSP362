cd0f71977d0b7770bd3728f94bc3b0287b42cadc
Alex Larioza <alexlarioza@gmail.com>
2013-05-24 19:31:27 -0700

    Fixed controller sensitivity

--- Assets/Scripts/Piece.js
+++ Assets/Scripts/Piece.js
@@ -12,8 +12,12 @@ var moveTimer:float;
 @HideInInspector
 var isSelected:boolean;
 
+@HideInInspector
+var canMovePiece:boolean;
+
 function Start ()
 {
+	canMovePiece = true;
 	moveTimer = speed;
 }
 
@@ -73,41 +77,54 @@ function Update ()
 				this.transform.Rotate(Vector3(0, 0, 90));
 			}
 		}
-	
+		
+		print(Input.GetAxis("Vertical") + "," + Input.GetAxis("Horizontal"));
 		// if moving left or right
 		if (this.velocity.x != 0)
 		{
-			if (Input.GetAxis("Vertical") > 0)
+			if (Input.GetAxis("Vertical") < 0 && canMovePiece)
 			{
+				canMovePiece = false;
 				if (!Collide(this.transform.position.x, this.transform.position.y + 1))
 				{
 					this.transform.position.y += 1;
 				}
 			}
-			else if (Input.GetAxis("Vertical") < 0)
+			else if (Input.GetAxis("Vertical") > 0 && canMovePiece)
 			{
+				canMovePiece = false;
 				if (!Collide(this.transform.position.x, this.transform.position.y - 1))
 				{
 					this.transform.position.y -= 1;
 				}
 			}
+			else if (Input.GetAxis("Vertical") == 0)
+			{
+				canMovePiece = true;
+			}
 		}
 		// if moving up or down
 		else if (this.velocity.y != 0)
 		{
-			if (Input.GetAxis("Horizontal")<0)
+			if (Input.GetAxis("Horizontal") < 0 && canMovePiece)
 			{
+				canMovePiece = false;
 				if (!Collide(this.transform.position.x - 1, this.transform.position.y))
 				{
 					this.transform.position.x -= 1;
 				}
 			}
-			else if (Input.GetAxis("Horizontal")>0)
+			else if (Input.GetAxis("Horizontal") > 0 && canMovePiece)
 			{
+			 	canMovePiece = false;
 				if (!Collide(this.transform.position.x + 1, this.transform.position.y))
 				{
 					this.transform.position.x += 1;
 				}
+			}
+			else if (Input.GetAxis("Horizontal") == 0)
+			{
+				canMovePiece = true;
 			}
 		}
 	}
