cf8d9ae82010bcc42bd5b2bc9dce456555d56a76
Patrick Rasmussen <mcman407@gmail.com>
2013-06-02 11:40:00 -0400

    Updated InGameHUD

--- Assets/Scripts/GameData.js
+++ Assets/Scripts/GameData.js
@@ -4,13 +4,26 @@ var score:int;
 var lines:int;
 var level:int;
 var linesRemaining:int;
+public var guiskin:GUISkin;
 
-function Start ()
-{
+    
+function Start (){
+
+}
+
+function Update (){
 
 }
 
-function Update ()
-{
-
-}
+function OnGUI(){
+	GUI.skin = guiskin;
+	GUI.Label(Rect(618, 661, 200, 20), "levelhere");
+	GUI.Label(Rect(810, 661, 200, 20), "levelname");
+	
+	GUI.Label(Rect(623, 694, 200, 20), "lineshere");
+	GUI.Label(Rect(817, 694, 200, 20), "linesremain");
+	
+	GUI.Label(Rect(628, 725, 200, 20), "scorehere"); //notice how the rect starts at 0/0 and the matrix handles the position
+	GUI.Label(Rect(828, 725, 200, 20), "hiscorehere");
+	
+   }
