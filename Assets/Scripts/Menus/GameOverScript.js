640f870f9035a3d4083d0a173bd0ae025c5a4643
patrick rasmussen <mcman407@gmail.com>
2013-06-07 01:10:29 -0400

    Gameover half finsihed
    needed to move inside :p

--- Assets/Scripts/Menus/GameOverScript.js
+++ Assets/Scripts/Menus/GameOverScript.js
@@ -3,26 +3,22 @@ var levelNames:String[];
 
 function Start ()
 {
-	var linesCleared:int; // total lines 
-	var multiplier:int; 
+	
+
 }
 
 function Update ()
 {
-
+	
 }
 
 function OnGUI()
 {
 	GUI.skin = guiskin;
 	var gameData:GameData = GameObject.Find("GameData").GetComponent(GameData) as GameData;
+	var finalScore = gameData.ringsTotal * gameData.score;
 	
-	GUI.Label(Rect(	400, 400, 800, 40), "Your total score was: " );
-	GUI.Label(Rect(810, 658, 200, 40), levelNames[gameData.level]);
-	
-	GUI.Label(Rect(623, 690, 200, 40), gameData.ringsTotal.ToString());
-	GUI.Label(Rect(817, 690, 200, 40), gameData.ringsRemaining.ToString());
-	
-	GUI.Label(Rect(628, 724, 200, 40), gameData.score.ToString()); //notice how the rect starts at 0/0 and the matrix handles the position
-	GUI.Label(Rect(828, 724, 200, 40), "hiscorehere");
+	GUI.Label(Rect(	335, 200, 800, 40), "You made it to level: " + gameData.level);
+	GUI.Label(Rect(	340, 240, 800, 40), "Your total score was ");
+	GUI.Label(Rect(	275, 280, 800, 40), "Total Rings : " + gameData.ringsTotal.ToString() + " x Gamescore: " + gameData.score + " = " + finalScore); 
 }
